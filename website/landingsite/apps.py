dcae117e9afe56dd366bb79944b3d93298792197
Zaman <zamanehsani@gmail.com>
2021-03-29 22:42:26 +0400

    some updates

--- website/landingsite/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class LandingsiteConfig(AppConfig):
-    name = 'landingsite'
--- website/landingsite/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from landingsite import views as site_views
-
-urlpatterns = [
-    path('', site_views.index, name="index"),
-]
